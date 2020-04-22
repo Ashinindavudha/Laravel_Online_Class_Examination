6901dd2c5d056b11533fb59b67f5d32377f0fcd5
AshinIndavudha <ashinindavudha2018@gmail.com>
2020-04-22 22:14:27 +0630

    User_edit_Completed_22_4_2020

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -59,40 +59,51 @@ class UserController extends Controller
     
     public function edit($id)
     {
-        $relations = [
-            'roles' => \App\Role::get()->pluck('title', 'id')->prepend('Please select', ''),
-        ];
+        // $relations = [
+        //     'roles' => \App\Role::get()->pluck('title', 'id')->prepend('Please select', ''),
+        // ];
 
         $user = User::findOrFail($id);
-
-        return view('users.edit', compact('user') + $relations);
+        $roles = Role::all()->pluck('title', 'id');
+        $course_semesters = CourseSemester::all()->pluck('name', 'id');
+        $student_registers = StudentRegister::all()->pluck('name', 'id');
+        $student_rolls = StudentRoll::all()->pluck('name', 'id');
+        $academic_years = AcademicYear::all()->pluck('name', 'id');
+        //dd($academic_years);
+        return view('admin.users.edit', compact(['roles', 'course_semesters', 'student_registers', 'student_rolls', 'academic_years', 'user']));
     }
 
     
-    public function update(UpdateUsersRequest $request, $id)
+    public function update(UpdateUserRequest $request, $id)
     {
         $user = User::findOrFail($id);
         $user->update($request->all());
-
-        return redirect()->route('users.index');
+        if ($request->input('photo', false)) {
+            if (!$user->photo || $request->input('photo') !== $user->photo->file_name) {
+                $user->addMedia(storage_path('tmp/uploads/' . $request->input('photo')))->toMediaCollection('photo');
+            }
+        } elseif ($user->photo) {
+            $user->photo->delete();
+        }
+        return redirect()->route('admin.users.index');
     }
 
     public function show($id)
     {
-        $relations = [
-            'roles' => \App\Role::get()->pluck('title', 'id')->prepend('Please select', ''),
-        ];
+        // $relations = [
+        //     'roles' => \App\Role::get()->pluck('title', 'id')->prepend('Please select', ''),
+        // ];
 
         $user = User::findOrFail($id);
 
-        return view('admin.users.show', compact('user') + $relations);
+        return view('admin.users.show', compact('user'));
     }
     public function destroy($id)
     {
         $user = User::findOrFail($id);
         $user->delete();
 
-        return redirect()->route('users.index');
+        return redirect()->route('admin.users.index');
     }
 
 }
